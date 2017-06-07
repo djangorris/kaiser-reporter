8fe4d6e371fda717b2f0508eea3282bd56e113e1
djangorris <lukkyjay@gmail.com>
2017-06-06 22:15:38 -0600

    it correctl counts the number of families and makes a list of the families - then it also sends a working email through my gmail now that I used my google application specific password

--- new_reporter.py
+++ new_reporter.py
@@ -12,9 +12,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.chrome.options import Options
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
 import time
 
-from secret import username, password
+from secret import kaiser_username, kaiser_password, host, port, username, password, from_email, to_list
 
 url = "https://clear.kaiserpermanente.org/?kp_shortcut_referrer=kp.org/clear#/login"
 driver = webdriver.Firefox()
@@ -23,10 +27,10 @@ driver.get(url)
 # elem1 = waiting.until(EC.element_to_be_clickable(('id','email')))
 time.sleep(4)
 email = driver.find_element_by_id('email')
-email.send_keys(username)
+email.send_keys(kaiser_username)
 time.sleep(1)
 pw = driver.find_element_by_id('password')
-pw.send_keys(password)
+pw.send_keys(kaiser_password)
 time.sleep(1)
 login = driver.find_element_by_xpath('//*[@id="signIn"]/form/div[3]/div[2]/button')
 login.click()
@@ -68,54 +72,34 @@ for client in clients:
     title = client.find_all('a', {'class': 'ng-binding'})[0].text.strip(" /n/t/r\n")
     if title not in client_list:
         client_list.append(title)
-time.sleep(1)
-print("There are " + str(len(client_list)) + " families.")
+num_clients = len(client_list)
+print("There are {} families.".format(num_clients))
 print(client_list)
 
-# client_set = set(client_list)
-# print(client_set)
-# num_clients = len(client_set_list)
-# print("There are " + str(num_clients) + " clients.")
+# try:
+email_conn = smtplib.SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(username, password)
+the_msg = MIMEMultipart('alternative')
+the_msg['Subject'] = "Kaiser client update"
+the_msg['From'] = from_email
 
 
-
-# images = []
-# for i in sel_soup.findAll("img"):
-#     src = i["src"]
-#     images.append(src)
-# print(images)
-# current_path = os.getcwd()
-# for img in images:
-#     try:
-#         file_name = os.path.basename(img)
-#         img_r = requests.get(img, stream=True)
-#         new_path = os.path.join(current_path, "images", file_name)
-#         with open(new_path, "wb") as output_file:
-#             shutil.copyfileobj(img_r.raw, output_file)
-#         del img_r
-#     except:
-#         pass
-
-# iterations = 0
-# while iterations < 10:
-#     html = driver.execute_script("return document.documentElement.outerHTML")
-#     sel_soup = BeautifulSoup(html, 'html.parser')
-#     print(len(sel_soup.findAll("img")))
-#     images = []
-#     for i in sel_soup.findAll("img"):
-#         src = i["src"]
-#         images.append(src)
-#     print(images)
-#     current_path = os.getcwd()
-#     for img in images:
-#         try:
-#             file_name = os.path.basename(img)
-#             img_r = requests.get(img, stream=True)
-#             new_path = os.path.join(current_path, "images", file_name)
-#             with open(new_path, "wb") as output_file:
-#                 shutil.copyfileobj(img_r.raw, output_file)
-#             del img_r
-#         except:
-#             pass
-#     iterations += 1
-#     time.sleep(5)
+plain_txt = "There are {} families.".format(num_clients)
+html_txt = '''\
+<html>
+    <head></head>
+    <body>
+        <p>"Testing."</p>
+    </body>
+</html>
+'''
+part_1 = MIMEText(plain_txt, 'plain')
+part_2 = MIMEText(html_txt, "html")
+the_msg.attach(part_1)
+the_msg.attach(part_2)
+email_conn.sendmail(from_email, to_list, the_msg.as_string())
+email_conn.quit()
+# except smtplib.SMTPException:
+#     print("error sending message")
